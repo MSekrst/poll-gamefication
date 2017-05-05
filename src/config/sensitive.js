ec432aa94b082878c9c0ab34c1279e9037028cae
Lara Dorčec <lara.dorcec@fer.hr>
2017-05-05 16:57:52 +0200

    Game added

---
+++ src/config/sensitive.js
@@ -0,0 +1,5 @@
+const databaseName = 'pollgamification';
+const username = 'ppa_poll';
+const password = 'ppa2017';
+
+export const mongoConnectionString = `mongodb://${username}:${password}@ds131061.mlab.com:31061/${databaseName}`;
