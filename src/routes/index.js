bd35a61f5f223fe35d2cd0ff5f74af73a9adb647
teapovic <teapovic@gmail.com>
2017-05-05 16:45:49 +0200

    anketa modal

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,10 @@ const router = Router();
 // home route HTML return
 router.get('/', (req, res) => {
   res.sendFile(resolve('public/html/home.html'));
+})
+
+router.get('/anketa', (req, res) => {
+  res.sendFile(resolve('public/html/anketa.html'));
 });
 
 export default router;
