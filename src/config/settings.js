e91a78e28bae4a9f886ae77d0e7ed8965f934574
MSekrst <matija.sekrst13@gmail.com>
2017-05-05 13:43:44 +0200

    database

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,2 +1,4 @@
 export const port = 3000;
 export const host = 'localhost';
+
+export const mongoConnectionString = 'mongodb://localhost:27017/ppa-game';
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { resolve } from 'path';
 
 const router = Router();
 
+// home route HTML return
 router.get('/', (req, res) => {
   res.sendFile(resolve('public/html/home.html'));
 });
