45190f4daacffd7e08c35a4ac4b01c44a48cbc97
MSekrst <matija.sekrst13@gmail.com>
2017-05-16 16:17:42 +0200

    user modal

--- public/js/user.js
+++ public/js/user.js
@@ -1,14 +1,41 @@
 'use strict';
 
 const userModal = $('#user-modal');
-const user = {};
+const user = { sex: '' };
+
+const handleSexChange = e => {
+  user.sex = e.target.value;
+};
+
+const validateUser = () => {
+  if (user.sex !== 'M' && user.sex !== 'F') return false;
+  if (isNaN(user.income) || user.income < 0) return false;
+  if (isNaN(user.age) || user.age < 6 || user.age > 150) return false;
+  if (user.status === '') return false;
+
+  return user.knowledge !== '';
+};
 
 userModal.modal('show');
 
+const alert = $('#validation-alert');
+
 $('#user-submit').on('click touchstart', e => {
-  startGame();
+  user.age = parseInt($('#age').val(), 10);
+  user.status = $('#status').val();
+  user.income = parseInt($('#income').val(), 10);
+  user.knowledge = $('#knowledge').val();
 
-  e.target.blur();
+  if (validateUser()) {
+    startGame();
 
-  userModal.modal('hide');
+    $.post('/user', user);
+
+    userModal.modal('hide');
+  } else {
+    alert.attr('style', 'display: block');
+    e.target.blur();
+  }
 });
+
+$('div.sex-container div.sex-wrapper input').change(handleSexChange);
