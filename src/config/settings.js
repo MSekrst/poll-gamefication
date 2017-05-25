a32019e2f5885a4968b7f17d44772d0cd329905e
MSekrst <matija.sekrst13@gmail.com>
2017-05-25 11:32:23 +0200

    sync script loading

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,5 +1,5 @@
 export const port = 3000;
 export const host = 'localhost';
 
-export const mongoConnectionString = `mongodb://localhost:27017/ppa`;
-//export const mongoConnectionString = `mongodb://user:user@ds131061.mlab.com:31061/pollgamification`;
+//export const mongoConnectionString = `mongodb://localhost:27017/ppa`;
+export const mongoConnectionString = `mongodb://user:user@ds131061.mlab.com:31061/pollgamification`;
